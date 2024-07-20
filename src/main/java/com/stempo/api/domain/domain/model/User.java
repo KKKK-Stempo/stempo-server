ff454996b0c29f49d8c8f4b776849a7912e881db
한관희 <noop103@naver.com>
2024-07-20 23:43:16 +0900

    feat(User): 회원 정보에 권한 추가

---
+++ src/main/java/com/stempo/api/domain/domain/model/User.java
@@ -0,0 +1,20 @@
+package com.stempo.api.domain.domain.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+
+    private String id;
+    private String password;
+    private Role role;
+}
--- src/main/java/com/stempo/api/domain/persistence/entity/UserEntity.java
+++ src/main/java/com/stempo/api/domain/persistence/entity/UserEntity.java
@@ -1,7 +1,10 @@
 package com.stempo.api.domain.persistence.entity;
 
+import com.stempo.api.domain.domain.model.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,4 +28,7 @@ public class UserEntity extends BaseEntity {
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
