7809b280703a7436986a798764fb2f8b565e9f56
한관희 <85067003+limehee@users.noreply.github.com>
2024-07-20 20:57:37 +0900

    테이블에 대한 영속성 객체 추가 (#8)

---
+++ src/main/java/com/stempo/api/domain/persistence/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package com.stempo.api.domain.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+}
