71106aba0a875e206606b1a8af2573ded330662c
한관희 <noop103@naver.com>
2024-07-24 21:29:15 +0900

    refactor(Video): id 매핑 오류 수정

--- src/main/java/com/stempo/api/domain/persistence/mappper/VideoMapper.java
+++ src/main/java/com/stempo/api/domain/persistence/mappper/VideoMapper.java
@@ -9,6 +9,7 @@ public class VideoMapper {
 
     public VideoEntity toEntity(Video video) {
         return VideoEntity.builder()
+                .id(video.getId())
                 .title(video.getTitle())
                 .content(video.getContent())
                 .thumbnailUrl(video.getThumbnailUrl())
@@ -19,6 +20,7 @@ public class VideoMapper {
 
     public Video toDomain(VideoEntity entity) {
         return Video.builder()
+                .id(entity.getId())
                 .title(entity.getTitle())
                 .content(entity.getContent())
                 .thumbnailUrl(entity.getThumbnailUrl())
