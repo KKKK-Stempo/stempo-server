ae747b259eb8a0cf573a9eba6711092cf50ca6cc
한관희 <noop103@naver.com>
2024-07-24 22:09:21 +0900

    refactor(Video): requestDto example 수정

--- src/main/java/com/stempo/api/domain/presentation/dto/request/VideoRequestDto.java
+++ src/main/java/com/stempo/api/domain/presentation/dto/request/VideoRequestDto.java
@@ -11,11 +11,11 @@ import lombok.Setter;
 public class VideoRequestDto {
 
     @NotNull
-    @Schema(description = "영상 제목", example = "영상 제목", minLength = 1, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "영상 제목", example = "리듬청각자극, 뇌성마비 환자 보행개선에 효과적 - (20120623_418회 방송)_내 몸이 찾는 음악", minLength = 1, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED)
     private String title;
 
     @NotNull
-    @Schema(description = "영상 내용", example = "영상 내용", minLength = 1, maxLength = 10000, requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "영상 내용", example = "조성래, 김수지 교수팀은 리듬청각자극이 뇌성마비 환자의 보행개선에도 효과가 있다는 연구결과를 발표했다.", minLength = 1, maxLength = 10000, requiredMode = Schema.RequiredMode.REQUIRED)
     private String content;
 
     @Schema(description = "썸네일 URL", example = "https://example.com/thumbnail.jpg", minLength = 1, maxLength = 255)
