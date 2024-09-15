a9a45ed14dd0e59ba680093ca1ad9432c159f454
한관희 <noop103@naver.com>
2024-09-15 18:33:19 +0900

    refactor(Rhythm): DTO 스키마 수정

--- src/main/java/com/stempo/api/domain/presentation/dto/request/RhythmRequestDto.java
+++ src/main/java/com/stempo/api/domain/presentation/dto/request/RhythmRequestDto.java
@@ -12,11 +12,11 @@ public class RhythmRequestDto {
 
     @NotNull(message = "BPM is required")
     @Range(min = 10, max = 200, message = "BPM must be between 10 and 200")
-    @Schema(description = "BPM", example = "60")
+    @Schema(description = "BPM", example = "60", minimum = "10", maximum = "200")
     private Integer bpm;
 
     @NotNull(message = "Bit is required")
     @Range(min = 1, max = 8, message = "Bit must be between 1 and 8")
-    @Schema(description = "Bit", example = "4")
+    @Schema(description = "Bit", example = "4", minimum = "1", maximum = "8")
     private Integer bit;
 }
