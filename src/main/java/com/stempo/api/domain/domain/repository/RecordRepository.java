8b59275780ccd1baac640a3860198be190d571f3
한관희 <noop103@naver.com>
2024-10-07 17:16:34 +0900

    refactor(Record): startDateTime 이전의 가장 최신 데이터가 없는 경우 정확도가 0인 데이터를 반환하도록 변경

--- src/main/java/com/stempo/api/domain/domain/repository/RecordRepository.java
+++ src/main/java/com/stempo/api/domain/domain/repository/RecordRepository.java
@@ -4,6 +4,7 @@ import com.stempo.api.domain.domain.model.Record;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface RecordRepository {
 
@@ -13,7 +14,7 @@ public interface RecordRepository {
 
     List<Record> findByDateBetween(String deviceTag, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
-    Record findLatestBeforeStartDate(String deviceTag, LocalDateTime startDateTime);
+    Optional<Record> findLatestBeforeStartDate(String deviceTag, LocalDateTime startDateTime);
 
     List<Record> findByDeviceTag(String deviceTag);
 
--- src/main/java/com/stempo/api/domain/presentation/dto/response/RecordResponseDto.java
+++ src/main/java/com/stempo/api/domain/presentation/dto/response/RecordResponseDto.java
@@ -14,20 +14,11 @@ public class RecordResponseDto {
     private Integer steps;
     private LocalDate date;
 
-    public static RecordResponseDto createDefault() {
+    public static RecordResponseDto create(Double accuracy, Integer duration, Integer steps, LocalDate date) {
         return RecordResponseDto.builder()
-                .accuracy(0.0)
-                .duration(0)
-                .steps(0)
-                .date(LocalDate.now())
-                .build();
-    }
-
-    public static RecordResponseDto create(String accuracy, String duration, String steps, LocalDate date) {
-        return RecordResponseDto.builder()
-                .accuracy(Double.parseDouble(accuracy))
-                .duration(Integer.parseInt(duration))
-                .steps(Integer.parseInt(steps))
+                .accuracy(accuracy)
+                .duration(duration)
+                .steps(steps)
                 .date(date)
                 .build();
     }
