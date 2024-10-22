01e04c80a2186a765f323ad777063f08069c5bf6
한관희 <noop103@naver.com>
2024-10-22 19:33:29 +0900

    feat(TotpAuthenticator): TOTP 테스트 코드 추가

---
+++ stempo-domain/src/test/java/com/stempo/model/TotpAuthenticatorTest.java
@@ -0,0 +1,22 @@
+package com.stempo.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TotpAuthenticatorTest {
+
+    private TotpAuthenticator totpAuthenticator;
+
+    @BeforeEach
+    void setUp() {
+        totpAuthenticator = TotpAuthenticator.create("DEVICE_TAG", "SECRET_KEY");
+    }
+
+    @Test
+    void TOTP가_정상적으로_생성되는지_확인한다() {
+        assertThat(totpAuthenticator.getDeviceTag()).isEqualTo("DEVICE_TAG");
+        assertThat(totpAuthenticator.getSecretKey()).isEqualTo("SECRET_KEY");
+    }
+}
