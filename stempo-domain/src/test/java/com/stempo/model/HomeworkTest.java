f249361f343ce2959cd6abb689c7c13ea68d18ff
한관희 <noop103@naver.com>
2024-10-22 19:56:44 +0900

    feat(Homework): 숙제 테스트 코드 추가

--- stempo-domain/src/test/java/com/stempo/model/HomeworkTest.java
+++ stempo-domain/src/test/java/com/stempo/model/HomeworkTest.java
@@ -37,4 +37,26 @@ class HomeworkTest {
         assertThat(homework.getDescription()).isEqualTo("UPDATED_DESCRIPTION");
         assertThat(homework.getCompleted()).isTrue();
     }
+
+    @Test
+    void 숙제_수정시_null_값을_무시한다() {
+        // given
+        Homework updateHomework = Homework.builder().build();  // 아무 값도 업데이트하지 않음
+
+        // when
+        homework.update(updateHomework);
+
+        // then
+        assertThat(homework.getDescription()).isEqualTo("DESCRIPTION");
+        assertThat(homework.getCompleted()).isFalse();
+    }
+
+    @Test
+    void 숙제의_완료_상태를_변경할_수_있다() {
+        // given
+        homework.setCompleted(true);
+
+        // then
+        assertThat(homework.getCompleted()).isTrue();
+    }
 }
