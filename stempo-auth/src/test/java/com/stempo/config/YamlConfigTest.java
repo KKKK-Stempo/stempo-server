45d99b8fcb58fa33187f2e4a62912e9a851cea2e
한관희 <noop103@naver.com>
2024-10-24 05:13:13 +0900

    feat(Auth): XssConfig 테스트 코드 추가

--- stempo-auth/src/test/java/com/stempo/config/YamlConfigTest.java
+++ stempo-auth/src/test/java/com/stempo/config/YamlConfigTest.java
@@ -19,16 +19,19 @@ class YamlConfigTest {
 
     @Test
     void yamlParser_빈이_정상적으로_생성된다() {
+        // then
         assertThat(yamlParser).isNotNull();
     }
 
     @Test
     void dumperOptions_빈이_정상적으로_생성된다() {
+        // then
         assertThat(dumperOptions).isNotNull();
     }
 
     @Test
     void dumperOptions_설정이_정상적으로_적용된다() {
+        // then
         assertThat(dumperOptions.getDefaultFlowStyle()).isEqualTo(DumperOptions.FlowStyle.BLOCK);
         assertThat(dumperOptions.getIndent()).isEqualTo(2);
         assertThat(dumperOptions.isPrettyFlow()).isTrue();
