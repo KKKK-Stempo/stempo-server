299f712ce034a92d3f33c6b921b7ca722ceb7826
한관희 <noop103@naver.com>
2024-10-24 04:57:29 +0900

    feat(Auth): XssEscapeServletFilter 테스트 코드 추가

--- stempo-auth/src/main/java/com/stempo/filter/XssEscapeServletFilter.java
+++ stempo-auth/src/main/java/com/stempo/filter/XssEscapeServletFilter.java
@@ -8,10 +8,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -20,9 +19,11 @@ public class XssEscapeServletFilter implements Filter {
     private final XssSanitizer xssSanitizer;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
-            HttpServletRequest sanitizedRequest = new XssEscapeServletRequestWrapper((HttpServletRequest) request, xssSanitizer);
+            HttpServletRequest sanitizedRequest = new XssEscapeServletRequestWrapper((HttpServletRequest) request,
+                    xssSanitizer);
             chain.doFilter(sanitizedRequest, response);
         } else {
             chain.doFilter(request, response);
