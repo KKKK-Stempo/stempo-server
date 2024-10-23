cd0ac967c54c19aa73c8f31e061863f2cea38ff8
한관희 <noop103@naver.com>
2024-10-24 01:27:11 +0900

    fix(Infra): JpaConfigTest 테스트 코드 수정

--- stempo-infrastructure/src/test/java/com/stempo/config/JpaConfigTest.java
+++ stempo-infrastructure/src/test/java/com/stempo/config/JpaConfigTest.java
@@ -2,13 +2,20 @@ package com.stempo.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.stempo.test.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest(classes = JpaConfig.class)
+@DataJpaTest
+@ContextConfiguration(classes = {TestConfig.class})
+@Import(JpaConfig.class)
+@ActiveProfiles("test")
 class JpaConfigTest {
 
     @Autowired
