6434069c29c5911cc5ee5dd271523e60a2aebef5
한관희 <noop103@naver.com>
2024-11-28 22:43:51 +0900

    feat: Spring AOT 적용

--- stempo-api/src/main/java/com/stempo/interceptor/ApiLoggingInterceptor.java
+++ stempo-api/src/main/java/com/stempo/interceptor/ApiLoggingInterceptor.java
@@ -3,7 +3,6 @@ package com.stempo.interceptor;
 import com.stempo.util.ApiLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,15 +12,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class ApiLoggingInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response,
-            @NotNull Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         request.setAttribute("startTime", System.currentTimeMillis());
         return true;
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, @NotNull Object handler,
-            Exception ex) {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                Exception ex) {
         ApiLogger.logRequestDuration(request, response, ex);
     }
 }
