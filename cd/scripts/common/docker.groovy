95c688daffd230be41cece51e328736660027817
한관희 <noop103@naver.com>
2025-02-28 17:45:34 +0900

    refactor(CD): docker.groovy, dockerUtils.groovy 통합

--- cd/scripts/common/docker.groovy
+++ cd/scripts/common/docker.groovy
@@ -1,5 +1,22 @@
 // scripts/common/docker.groovy
 /**
+ * Docker Hub에 로그인하는 함수.
+ *
+ * @param credentialsId : Jenkins 크리덴셜 ID (String)
+ */
+def dockerLogin(Map params = [:]) {
+    if (!params.credentialsId) {
+        error "dockerLogin: credentialsId 파라미터가 필요합니다."
+    }
+    withCredentials([usernamePassword(credentialsId: params.credentialsId, usernameVariable: 'DOCKER_HUB_USER', passwordVariable: 'DOCKER_HUB_PASSWORD')]) {
+        sh """
+            echo "Logging in to Docker Hub..."
+            echo \$DOCKER_HUB_PASSWORD | docker login -u \$DOCKER_HUB_USER --password-stdin
+        """
+    }
+}
+
+/**
  * 공통 도커 이미지 빌드 및 푸시 함수.
  *
  * 이 함수는 Blue-Green 배포와 Rolling 배포 모두에서 사용할 수 있습니다.
