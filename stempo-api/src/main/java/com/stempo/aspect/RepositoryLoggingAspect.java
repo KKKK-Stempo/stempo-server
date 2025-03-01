ddc3c65d6da0be4e80d931fcaaef54fad893c1a5
한관희 <noop103@naver.com>
2025-03-02 01:28:11 +0900

    feat(Deps): MDC에 서비스 로직, DB의 수행시간 추가

---
+++ stempo-api/src/main/java/com/stempo/aspect/RepositoryLoggingAspect.java
@@ -0,0 +1,25 @@
+package com.stempo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class RepositoryLoggingAspect {
+
+    @Around("execution(* com.stempo.repository..*(..))")
+    public Object logRepositoryExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long duration = System.currentTimeMillis() - start;
+            MDC.put("repositoryExecutionTime", String.valueOf(duration));
+        }
+    }
+}
