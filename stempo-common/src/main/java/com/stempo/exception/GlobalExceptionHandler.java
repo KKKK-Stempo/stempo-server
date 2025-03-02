8b3e6e41eab9875991d1d94291c79297c3163a77
한관희 <noop103@naver.com>
2025-03-02 11:42:02 +0900

    refactor(Logging): 예외 핸들러 MDC 정보 추가

--- stempo-common/src/main/java/com/stempo/exception/GlobalExceptionHandler.java
+++ stempo-common/src/main/java/com/stempo/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.stempo.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,16 +15,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ErrorResponse<Exception> handleBaseException(HttpServletResponse response, BaseException ex) {
         response.setStatus(ex.getErrorCode().getStatus().value());
+        MDC.put("ErrorCode", ex.getErrorCode().name());
+        MDC.put("exceptionMessage", ex.getMessage());
         return ErrorResponse.failure(ex);
     }
 
     @ExceptionHandler(Exception.class)
     public ErrorResponse<Exception> handleServerError(HttpServletRequest request, HttpServletResponse response,
-            Exception ex) {
+        Exception ex) {
         ErrorCode errorCode = ExceptionMapper.getErrorCode(ex);
         response.setStatus(errorCode.getStatus().value());
-        log.error("Server error occurred.", ex);
-        log.error("Error message: {}", ex.getMessage());
+        MDC.put("exceptionClass", ex.getClass().getName());
+        MDC.put("ErrorCode", errorCode.name());
+        MDC.put("exceptionMessage", errorCode.getDefaultMessage());
+        if (ex.getStackTrace().length > 0) {
+            MDC.put("exceptionAt", ex.getStackTrace()[0].toString());
+        }
         return ErrorResponse.failure(new BaseException(errorCode, ex.getMessage()));
     }
 }
