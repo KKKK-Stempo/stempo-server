ed0e28019483027d19fc78460425d41ea5bf24c2
한관희 <noop103@naver.com>
2025-03-03 02:45:14 +0900

    feat(Loggging): 비동기 환경에서 MDC가 자동으로 전파되도록 설정

---
+++ stempo-common/src/main/java/com/stempo/config/MdcTaskDecorator.java
@@ -0,0 +1,24 @@
+package com.stempo.config;
+
+import java.util.Map;
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+
+public class MdcTaskDecorator implements TaskDecorator {
+
+    @Override
+    public Runnable decorate(Runnable runnable) {
+        // 현재 스레드의 MDC 컨텍스트 복사
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+        return () -> {
+            try {
+                if (contextMap != null) {
+                    MDC.setContextMap(contextMap);
+                }
+                runnable.run();
+            } finally {
+                MDC.clear();
+            }
+        };
+    }
+}
